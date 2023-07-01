0b2d04850c62f9c4fef2fbeaa8ba737a3332befb
jiyongnoh <njy99000@naver.com>
2023-07-01 15:01:30 +0900

    deploy: 배포수정

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
+const debug = process.env.NODE_ENV !== "production";
+const repository = "http://jiyongnoh.github.io";
+
 const nextConfig = {
   reactStrictMode: true,
-  // 외부 이미지 도메인 설정(안하면 못씀)
-  // images: {
-  //   domains: ["cdn.pixabay.com"],
-  // },
-  basePath: "/Chatting-Web---Next.js",
+  assetPrefix: !debug ? `/${repository}/` : "", // production 일때 prefix 경로
+  trailingSlash: true, // 빌드 시 폴더 구조 그대로 생성하도록
 };
 
 module.exports = nextConfig;
