ff4e35bcdb76b3817251940eee56b408838cd2c1
jiyongnoh <njy99000@naver.com>
2023-07-01 15:30:01 +0900

    fix: 이미지 최적화 수정

--- component/Goods.js
+++ component/Goods.js
@@ -1,11 +1,11 @@
 import Link from "next/link";
-import Image from "next/image";
+// import Image from "next/image";
 import style from "@/styles/Goods.module.css";
 
 export default function Goods({ item }) {
   return (
     <div className={style.container}>
-      <Image src={item.img} width={300} height={300} alt="dummy" />
+      <img src={item.img} width={300} height={300} alt="dummy" />
       <div>
         <Link href={`/list/detail/${item.id}`}>
           <div>{item.name}:path</div>
