6c31dd9cc5a341b9c039992a77a68c2504cdd717
jiyongnoh <njy99000@naver.com>
2023-07-04 15:05:39 +0900

    fix: netlify 배포 정보 수정

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
+const debug = process.env.NODE_ENV !== "production";
+const name = "https://jiyongnoh.github.io";
 
 const nextConfig = {
   reactStrictMode: true,
   basePath: "/Chatting-Web---Next.js",
+  assetPrefix: !debug ? `/${name}/` : "",
 };
 
 module.exports = nextConfig;
--- pages/api/chat/socketio.js
+++ pages/api/chat/socketio.js
@@ -3,8 +3,8 @@ import { Server } from "socket.io";
 export default async (req, res) => {
   if (!res.socket.server.io) {
     console.log("New Socket.io server...✅");
-
     const httpServer = res.socket.server;
+
     const io = new Server(httpServer, {
       path: "/Chatting-Web---Next.js/api/chat/socketio",
     });
