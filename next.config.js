334d8ac8ccba0c9d013e78513d1d1e4a1bff3387
jiyongnoh <njy99000@naver.com>
2023-07-04 14:24:57 +0900

    Updates

--- next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-
-const nextConfig = {
-  reactStrictMode: true,
-  basePath: "/Chatting-Web---Next.js",
-};
-
-module.exports = nextConfig;
--- pages/api/hello.js
+++
@@ -1,5 +0,0 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-// 서버 기능을 담당하는 파일
-export default function handler(req, res) {
-  res.status(200).json({ name: "John Doe" });
-}
